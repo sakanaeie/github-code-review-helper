56a3bb9dc9eece25e4e44b3f3d64d813f9f72dc2
sakanaeie <sakanaeie@gmail.com>
2015-06-20 04:19:48 +0900

    リファクタリング

--- app/onupdated.js
+++ app/onupdated.js
@@ -1,95 +1,64 @@
-// generate tab transfer button
+// tab文字変換ボタンを生成する
 (function() {
   var div_id         = 'GithubCodeReviewHelper-tab-transfer-div';
   var button_id_soft = 'GithubCodeReviewHelper-tab-transfer-soft-button';
   var button_id_hard = 'GithubCodeReviewHelper-tab-transfer-hard-button';
 
-  var div = document.getElementById(div_id);
-  if (null === div) {
-    // gen soft button
-    var button_soft = document.createElement('button');
-    button_soft.id        = button_id_soft;
-    button_soft.className = 'btn btn-sm';
-    button_soft.innerHTML = 'Soft';
-    button_soft.onclick   = function() {
+  if (0 === $('#' + div_id).length) {
+    // Softボタンを生成する
+    var button_soft = $('<button>').attr('id', button_id_soft).attr('class', 'btn btn-sm').html('Soft').click(function() {
       var converter = new TabConverter();
       converter.execSoft();
-    };
+    });
 
-    // gen hard button
-    var button_hard = document.createElement('button');
-    button_hard.id        = button_id_hard;
-    button_hard.className = 'btn btn-sm';
-    button_hard.innerHTML = 'Hard';
-    button_hard.onclick   = function() {
+    // Hardボタンを生成する
+    var button_hard = $('<button>').attr('id', button_id_hard).attr('class', 'btn btn-sm').html('Hard').click(function() {
       var converter = new TabConverter();
       converter.execHard();
-    };
+    });
 
-    // gen div
-    div = document.createElement('div');
-    div.id        = div_id;
-    div.innerHTML = 'GithubCodeReviewHelper tab transfer :'
-
-    // add button to div
-    div.appendChild(button_soft);
-    div.appendChild(button_hard);
-
-    // add div to body
-    var body = document.getElementById('js-repo-pjax-container');
-    body.insertBefore(document.createElement('hr'), body.firstChild);
-    body.insertBefore(div, body.firstChild);
+    // ボタンを配置する
+    $('#js-repo-pjax-container').prepend(
+      $('<div>').attr('id', div_id).html('GithubCodeReviewHelper tab transfer :').append(
+        button_soft,
+        button_hard
+      ),
+      $('<hr>')
+    );
   }
 })();
 
-// generate page top button
+// PageTopボタンを生成する
 (function() {
   var button_id = 'GithubCodeReviewHelper-page-top-button';
-  var button    = document.getElementById(button_id);
-  if (null === button) {
-    var elm = document.createElement('button');
-    elm.id        = button_id;
-    elm.className = 'btn btn-sm';
-    elm.innerHTML = 'Page Top';
-    elm.onclick   = function() {
-      scrollTo(0, 0);
-    };
-    document.getElementById('js-repo-pjax-container').appendChild(elm);
+
+  if (0 === $('#' + button_id).length) {
+    $('#js-repo-pjax-container').append(
+      $('<button>').attr('id', button_id).attr('class', 'btn btn-sm').html('Page top').click(function() {
+        scrollTo(0, 0);
+      })
+    );
   }
 })();
 
-// resize LGTM
+// LGTM画像をリサイズする
 (function() {
-  var imgs  = document.getElementsByTagName('img');
-  var count = imgs.length;
-  for (var i = 0; i < count; i++) {
-    if ('LGTM' === imgs[i].alt) {
-      imgs[i].style.height = '200px';
+  $('img').each(function() {
+    if ('LGTM' === $(this).attr('alt')) {
+      $(this).css('height', '200px');
     }
-  }
+  });
 })();
 
-// silence jenkinsbot
+// botの発言を非表示にする
 (function() {
-  var comments = document.getElementsByClassName('timeline-comment-wrapper');
-  var count    = comments.length;
-  for (var i = 0; i < count; i++) {
-    if (undefined === comments[i]) {
-      continue;
-    }
-
-    var childs = comments[i].childNodes;
-    if (undefined === childs) {
-      continue;
-    }
-
-    var child_count = childs.length;
-    for (var j = 0; j < child_count; j++) {
-      var elm = childs[j];
-      if (undefined !== elm.href && -1 !== elm.href.indexOf('jenkinsbot')) {
-        comments[i].parentNode.removeChild(comments[i]);
-        continue;
+  $('.timeline-comment-wrapper').each(function() {
+    var author = $(this).find('.author');
+    if (0 !== author.length) {
+      var name = $(author[0]).html();
+      if (-1 !== name.indexOf('jenkinsbot') || -1 !== name.search(/-bot$/)) {
+        $(this).remove();
       }
     }
-  }
+  });
 })();
--- app/popup.js
+++ app/popup.js
@@ -1,37 +1,45 @@
-var displayLGTM = function() {
-  var div = $('#lgtm-list');
+(function() {
+  var displayLGTM = function() {
+    var div = $('#lgtm-list');
 
-  // 既存の画像を削除する
-  div.empty();
+    // 既存の画像を削除する
+    div.empty();
 
-  // LGTM画像を取得し、タグを生成する
-  var count = 0;
-  for (var i = 0; i < 3; i++) {
-    $('#lgtm-refresh-animate').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate');
+    // LGTM画像を取得し、タグを生成する
+    var count = 0;
+    for (var i = 0; i < 3; i++) {
+      // くるくるを表示させる
+      $('#lgtm-refresh-animate').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate');
 
-    $.getJSON('http://www.lgtm.in/g', function(json) {
-      var md = '![LGTM](' + json.imageUrl + ')';
-      div.append($('<hr>'));
-      div.append($('<img>').attr('class', 'lgtm-img img-thumbnail').attr('src', json.imageUrl)
-        .click(function() {
+      // 画像タグの生成器を作成する
+      var imgGen = function(lgtm_json) {
+        return $('<img>').attr('class', 'lgtm-img img-thumbnail').attr('src', lgtm_json.imageUrl).click(function() {
+          // クリックしたとき、アクティブなタブにメッセージを送信する
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {process: 'lgtm', markdown: md}, function(response) {});
+            chrome.tabs.sendMessage(tabs[0].id, {process: 'lgtm', markdown: '![LGTM](' + lgtm_json.imageUrl + ')'}, function(response) {});
           });
-        })
-        .load(function() {
+        }).load(function() {
+          // ロード完了したとき
           if (3 <= ++count) {
+            // くるくるを消す
             $('#lgtm-refresh-animate').removeClass('glyphicon glyphicon-refresh glyphicon-refresh-animate');
           }
-        })
-      );
-    });
-  }
-};
+        });
+      };
 
-$(window).load(function () {
-  // リロードボタンにバインドする
-  $('#lgtm-reload').click(displayLGTM);
+      // ajax
+      $.getJSON('http://www.lgtm.in/g', function(json) {
+        div.append($('<hr>'));
+        div.append(imgGen(json));
+      });
+    }
+  };
 
-  // 一度実行する
-  displayLGTM();
-});
+  $(window).load(function () {
+    // リロードボタンにバインドする
+    $('#lgtm-reload').click(displayLGTM);
+
+    // 一度実行する
+    displayLGTM();
+  });
+})();
