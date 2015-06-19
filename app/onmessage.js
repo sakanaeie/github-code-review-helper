0a4b433f910ec5b8f143285ed1eb2c203ad4e410
sakanaeie <sakanaeie@gmail.com>
2015-06-19 17:44:58 +0900

    typo修正

--- app/onmessage.js
+++ app/onmessage.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if ('lgmt' === request.process) {
+  if ('lgtm' === request.process) {
     $('#new_comment_field').val($('#new_comment_field').val() + request.markdown);
   }
 });
