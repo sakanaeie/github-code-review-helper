f51dbbe4f8238170cb039439ee07080a2d0ea6c2
sakanaeie <sakanaeie@gmail.com>
2015-06-21 21:49:34 +0900

    LGTMをクリップボードにコピーするようにした

--- app/popup.js
+++ app/popup.js
@@ -11,26 +11,42 @@
       // くるくるを表示させる
       $('#lgtm-refresh-animate').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate');
 
-      // 画像タグの生成器を作成する
-      var imgGen = function(lgtm_json) {
-        return $('<img>').attr('class', 'lgtm-img img-thumbnail').attr('src', lgtm_json.imageUrl).click(function() {
-          // クリックしたとき、アクティブなタブにメッセージを送信する
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {process: 'lgtm', markdown: '![LGTM](' + lgtm_json.imageUrl + ')'}, function(response) {});
-          });
-        }).load(function() {
-          // ロード完了したとき
-          if (3 <= ++count) {
-            // くるくるを消す
-            $('#lgtm-refresh-animate').removeClass('glyphicon glyphicon-refresh glyphicon-refresh-animate');
-          }
-        });
-      };
-
       // ajax
       $.getJSON('http://www.lgtm.in/g', function(json) {
-        div.append($('<hr>'));
-        div.append(imgGen(json));
+        var md = '![LGTM](' + json.imageUrl + ')';
+
+        // 画像を配置する
+        div.append(
+          $('<hr>'),
+          $('<div>').append(
+            $('<img>').attr('class', 'lgtm-img img-thumbnail').attr('src', json.imageUrl).click(function() {
+              // アクティブなタブにメッセージを送信する
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {process: 'lgtm', markdown: md}, function(response) {});
+              });
+
+              // クリップボードにコピーする
+              var input = $('<input>').val(md);
+              $('body').append(input);
+              input.select();
+              var copied = document.execCommand('copy');
+              input.remove();
+
+              var flash = $('<div>').attr('class', 'text-primary').css('margin-left', '4px').html(copied ? '↓ Copied to clipboard' : 'Failed copy to clipboard');
+              $(this).parent().prepend(flash);
+              setTimeout(function() {
+                flash.hide('slow', function() {
+                  $(this).remove();
+                });
+              }, 3000);
+            }).load(function() {
+              if (3 <= ++count) {
+                // くるくるを消す
+                $('#lgtm-refresh-animate').removeClass('glyphicon glyphicon-refresh glyphicon-refresh-animate');
+              }
+            })
+          )
+        );
       });
     }
   };
